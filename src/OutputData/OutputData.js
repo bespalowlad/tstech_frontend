cdf388db2e07be05964188e17814f5e5c37d3955
Vladyslav <bespalov.v@brights.cloud>
2020-08-16 13:43:20 +0300

    Add table layout for outoutData

--- src/OutputData/OutputData.js
+++ src/OutputData/OutputData.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Table } from 'react-bootstrap'
 
 const OutputData = ({ currentDataForm }) => (
-    <div>{JSON.stringify(currentDataForm)}</div>
+    <Table striped bordered hover className="mt-5">
+        <thead>
+            <tr>
+                <th>Prority</th>
+                <th>Property</th>
+                <th>Order</th>
+            </tr>
+        </thead>
+        <tbody>
+            {currentDataForm.map(item => (
+                <tr key={item.priority}>
+                    <td>{item.priority}</td>
+                    <td>{item.property}</td>
+                    <td>{item.order}</td>
+                </tr>
+            ))}
+        </tbody>
+    </Table>
 )
 
 export default OutputData
